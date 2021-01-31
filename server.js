c318be83d139ff1b64c68ea04214cb71cdf9117e
Glitch (desert-faceted-mollusk) <none>
2021-01-31 11:12:21 +0000

    ⛈✋ Checkpoint ./server.js:27715651/710

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ console.log('By black jack');/////BLACK JACK
 console.log('♔♔♔♔♔♔♔♔♔♔♔♔♔♔♔♔♔♔♔');
 console.log(`Logged in as ${client.user.tag}!`);
 client.user.setActivity(`Partner Bot|ڕیکلام بۆ من بنێرە `)///////shtek bnwsa
-client.user.setStatus("Shex Balen")/////ba dly xot statusek bnwsa
+client.user.setStatus("dnd")/////ba dly xot statusek bnwsa
 console.log(`Logined`)
 })
 
@@ -35,12 +35,39 @@ console.log(`Logined`)
 client.on("message", message => {
 if (message.channel.type === "dm") {
 if(message.content.startsWith("https://discord.gg")) 
-client.channels.cache.get("id channel").send(`<@${message.author.id}> ${message.content}`)
-message.author.send("**Done chek partner**")
-message.author.send("<#id channel>")
+client.channels.cache.get("803526086604095497").send(`<@${message.author.id}> ${message.content}`)
+message.author.send("**https://discord.gg/VjRd5VfJCr Done chek partner**")
+message.author.send("<803526086604095497>")
 }
 });
 
+client.on('message', msg => {
+    if (msg.content === 'Reklam akan') {
+      msg.reply('BOM BNERA');
+    }
+  });
+client.on('message', msg => {
+    if (msg.content === 'reklam haya') {
+      msg.reply('BOM BNERA');
+    }
+  });
 
+client.on('message', msg => {
+    if (msg.content === 'Slaw reklam agornawa') {
+      msg.reply('BOM BNERA');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'ڕیکلام ئەگۆڕنەوە') {
+      msg.reply('بۆ منی بنێرە گیان');
+    }
+  });
+
+client.on('message', msg => {
+    if (msg.content === 'سڵاو  ڕیکلام ئەگۆڕنەوە') {
+      msg.reply('بنێرە بۆ من دڵم');
+    }
+  });
   
 client.login(process.env.TOKEN);
