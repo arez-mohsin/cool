a5f73e6dddc09ec42b1fcb2be54314bb6c3b3036
Glitch (desert-faceted-mollusk) <none>
2021-01-31 13:25:45 +0000

    👼🏌 Checkpoint ./server.js:28036817/787

--- server.js
+++ server.js
@@ -32,11 +32,9 @@ client.on("ready", async () => {
 client.on("message", message => {
 if (message.channel.type === "dm") {
 if(message.content.startsWith("https://discord.gg")) 
-message.author.send("<#803526086604095497>")
-message.author.send("**Done check partner https://discord.gg/WYBp6UPDUK**")
-message.author.send(`<@${message.author.id}>`)
 client.channels.cache.get("803526086604095497").send(`<@${message.author.id}> ${message.content}`)
-
+message.author.send("**Done chek partner https://discord.gg/DGTHj7hR**")
+message.author.send("<#803526086604095497>")
 }
 });
 
