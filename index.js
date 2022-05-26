4e3988f2e311a4fc057b174c6fa42ec401e34467
Glitch (desert-faceted-mollusk) <none>
2022-05-26 10:40:53 +0000

    🍁🍄 Checkpoint ./index.js:23877998/1139

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log("بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ");
   client.user.setPresence({
-    activities: [{ name: `${prefix}help | Yato Bot Version 13` }],
+    activities: [{ name: `${prefix}help | Long Live MrBlackJack` }],
     status: "dnd"
   });
   console.log(`Logined`);
@@ -50,8 +50,30 @@ trusted: "Off"
   .setFooter(message.guild.name)
   message.channel.send({embeds: [embed]})     
 trust[member.id].trusted = "On";
-}}
-})
+saveList()
+}
+}})
+ client.on("messageCreate", message => { 
+if(message.content === prefix + "remove"){
+  if(message.author.id !== message.guild.ownerId) return message.reply("This Command Just Owner Ship Can Use It")
+let args = message.content.split(" ").slice(1).join("")
+let member = message.mentions.users.first()
+if(!args) return message.reply("Please Mention Member To Add Trust List")
+if(!member) return message.reply("Member Not Founded")
+if(!trust[member.id])
+trust[member.id] = {
+trusted: "Off"
+}      
+  if(trust[member.id].Blacklist === "Off") {
+    let embed = new MessageEmbed()
+  .setTitle("Done Added on Trusted List")
+  .addField("Member", member.toString())
+  .setFooter(message.guild.name)
+  message.channel.send({embeds: [embed]})     
+trust[member.id].trusted = "Off";
+saveList()
+  }
+}})
 
 
 
