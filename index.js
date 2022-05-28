edc283c756dc27d05452aa40626943e2f0c39a46
Glitch (desert-faceted-mollusk) <none>
2022-05-28 12:14:11 +0000

    📕🐠 Checkpoint ./index.js:23877998/145

--- index.js
+++ index.js
@@ -111,9 +111,7 @@ trusted: "Off"
  if(executor.id === channel.guild.ownerId) return
   if(trust === true) return
 let adminRole = channel.guild.roles.cache.find(r => r.permissions.has('ADMINISTRATOR' || 'MANAGE_CHANNELS'))
-  adminRole.role.managed.remove(executor.id, { 
-    reason: "Anti Channel Delete"
-  })
+  executor.roleManager.remove(adminRole.id, {reason: "Channel Delete"})
 let log = channel.guild.channels.cache.find(c => c.name === "log")
  if(!log) return
 log.send(`The ${executor.id} Channel Is Deleting`) 
